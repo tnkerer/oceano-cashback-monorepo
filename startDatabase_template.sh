d9567ecd4f226666d9c070bedfb987cc0bbd38ac
menezesphill <phillipe.o.m@hotmail.com>
2023-01-03 18:17:08 -0300

    database script

---
+++ startDatabase_template.sh
@@ -0,0 +1,3 @@
+# Do a Docker run and remove the container if it already exists
+
+docker run -d --name my-postgres-db -p 5432:5432 -e POSTGRES_DB=$DB_NAME -e POSTGRES_USER=$ADMIN_NAME -e POSTGRES_PASSWORD=$ADMIN_PASSWORD -v /custom/mount:/var/lib/postgre_sql/data postgres:alpine
