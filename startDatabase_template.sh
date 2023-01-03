6393127ff42174dd0cfa49d03052046bb3953122
menezesphill <phillipe.o.m@hotmail.com>
2023-01-03 19:03:37 -0300

    feat: updated server

--- startDatabase_template.sh
+++ startDatabase_template.sh
@@ -1,3 +1,5 @@
 # Do a Docker run and remove the container if it already exists
 
 docker run -d --name my-postgres-db -p 5432:5432 -e POSTGRES_DB=$DB_NAME -e POSTGRES_USER=$ADMIN_NAME -e POSTGRES_PASSWORD=$ADMIN_PASSWORD -v /custom/mount:/var/lib/postgre_sql/data postgres:alpine
+
+docker run -d --name api-server -p 3001:3001 xdc-server
