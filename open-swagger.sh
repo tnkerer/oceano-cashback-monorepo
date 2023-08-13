2e0612ac059930267d41e85e30b904da6b7a16f9
tnkerer <phillipe.o.m@hotmail.com>
2023-08-13 01:51:23 -0300

    chore: updated gitpod yaml

--- open-swagger.sh
+++ open-swagger.sh
@@ -1,6 +1,12 @@
-while ! nc -z localhost 3001; do
+#!/bin/bash
+
+# Read the API_URL environment variable
+SWAGGER_URL="$SWAGGER_URL"
+
+# Wait for the port to be accessible
+while ! ncat -z localhost 3001; do
   sleep 1
 done
 
-# Open the browser to the specific path
-xdg-open http://localhost:3001/swagger
+# Open the browser to the specified URL
+xdg-open "$SWAGGER_URL"
